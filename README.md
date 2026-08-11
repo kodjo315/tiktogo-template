5cb15669575c9b3e1c0399831266c65aa1a87913
kodjo315 <kodjoamo9@gmail.com>
2026-08-11 22:42:32 +0000

    Update and rename README.md to Zapp/page.js

--- README.md
+++
@@ -1,2 +0,0 @@
-# tiktogo-template
-Template tiktogo 
---
+++ Zapp/page.js
@@ -0,0 +1,10 @@
+# tiktogo-template
+Template tiktogo 
+export default function Home() {
+  return (
+    <main style={{padding: 50, textAlign: 'center', background: 'black', color: 'white', height: '100vh', fontSize: 24}}>
+      <h1>🔥 TIKTOGO 🔥</h1>
+      <p>Prochain TikTok du Togo !</p>
+    </main>
+  )
+}
